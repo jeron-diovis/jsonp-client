e35f9f53da27a45ec00dcd5775ad51f756d84e27
Andrey Bobukh <andrey.bobukh@mediasapiens.co>
2015-08-27 21:33:12 +0300

    remove tm file

--- serv.js
+++
@@ -1,9 +0,0 @@
-var fs = require('fs');
-var express = require('express');
-var server = express();
-server.get("/fixtures/:file", function(req, res) {
-	var content = fs.readFileSync('./test/fixtures/' + req.params.file);
-	res.setHeader("Contet-Type", "text/plain");
-	res.status(200).end(content);
-});
-server.listen(8020, "localhost");
